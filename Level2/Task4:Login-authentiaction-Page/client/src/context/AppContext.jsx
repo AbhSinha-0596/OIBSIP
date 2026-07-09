c8d35fd5d2b5ad87a53097e3ab03a3e9e7c0b4a8
AbhijitSinha <abhijitcoding0508@gmail.com>
2026-07-09 15:50:18 +0530

    Implement AppContext for authentication management

---
+++ Level2/Task4:Login-authentiaction-Page/client/src/context/AppContext.jsx
@@ -0,0 +1,49 @@
+import React,{createContext, useEffect} from "react";
+import axios from 'axios';
+import {toast} from 'react-toastify';
+
+export const AppContent = createContext()
+
+export const AppContextProvider = (props)=>
+{
+    axios.defaults.withCredentials=true;
+    const backendUrl=import.meta.env.VITE_BACKEND_URL 
+    const[isLoggedIn, setIsLoggedIn]=React.useState(false)
+    const[userData, setUserData]=React.useState(false)
+
+    const getAuthState = async()=>{
+        try{
+            const {data}=await axios.get(backendUrl+'/api/auth/is-auth')
+            if(data.success){
+                setIsLoggedIn(true)
+                getUserData()
+            }
+        }catch(error)
+        {
+            toast.error(error.message)
+        }
+    }
+    const getUserData=async ()=>{
+        try{
+            const {data}=await axios.get(backendUrl +'/api/user/data')
+            data.success? setUserData(data.userData):toast.error(data.message)
+        }catch(error)
+        {
+            toast.error(error.message);
+        }
+    }
+    useEffect(()=>{
+        getAuthState();
+    },[])
+    const value={
+        backendUrl,
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData,
+        getUserData
+    }
+    return(
+        <AppContent.Provider value={value}>
+            {props.children}
+        </AppContent.Provider>
+    )
+}
